d240641f0c1dbb31f40cb9848508a292334616a1
tagoad <43684949+tagoad@users.noreply.github.com>
2022-04-02 20:52:13 -0500

    Swapped Packages to Snowpack

---
+++ build/js/main.js
@@ -0,0 +1,4 @@
+import ExternalServices from "./externalServices.js";
+import { getLocalStorage } from "./util.js";
+
+const externalServices = new ExternalServices();
---
+++ snowpack.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  plugins: [["@snowpack/plugin-sass"]],
+  mount: {
+    src: "/",
+  },
+  buildOptions: {
+    out: "build",
+    baseUrl: "/papyrus/build/",
+    clean: true,
+    htmlFragments: true,
+  },
+  experiments: {
+    optimize: {
+      // 'bundle': true,
+      minify: true,
+      target: "es2015",
+    },
+  }
+};
